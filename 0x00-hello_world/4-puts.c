897959a1b79b22c62cb9884a1fedad19a80c79cf
delexbrainz <alukobamidele@gmail.com>
2022-09-12 13:33:40 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 * The string value is assigned to it
 * puts function is used to print out the string in str
 * Return: (0) Success
-*/ 
+*/
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
