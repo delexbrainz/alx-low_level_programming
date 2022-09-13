99ad7727d31b25284a1fc572460c0c97f7a3f983
delexbrainz <alukobamidele@gmail.com>
2022-09-13 01:11:31 -0700

    print alphabet without some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		if (alph == 'e')
+		{
+			alpha++;
+		}
+		else if (alph == 'q')
+		{
+			alpha++;
+		}
+		else 
+		{
+			putchar(alph);
+			alph++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
