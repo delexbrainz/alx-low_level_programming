1c1a9176f00e7a5fb527637f4ceb5c0e53cc92f3
delexbrainz <alukobamidele@gmail.com>
2022-09-13 02:16:47 -0700

    print alphabet backward

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char alph;
+
+	alph = 'z';
+	while (alph >= 'a')
+	{
+		putchar(alph0);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
