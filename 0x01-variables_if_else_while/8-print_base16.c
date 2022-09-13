98e63ef68023d9f2bc89703d329a93a827720ee2
delexbrainz <alukobamidele@gmail.com>
2022-09-13 02:30:41 -0700

    alpha numeric print

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int a;
+	char alph;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(d);
+	for (alph = 'a'; alph <= 'f'; alph++)
+		putchar(alph);
+	putchar('\n');
+	return (0);
+}
