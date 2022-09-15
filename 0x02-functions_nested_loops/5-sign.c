60fbb352cafc12492d2eb87a9a0073c0d2f8085f
delexbrainz <alukobamidele@gmail.com>
2022-09-15 11:13:55 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - check if number is positive or negative
+ * @n: input to be checked
+ *
+ * Return : 1 if positive, 0 if input is zero, negative if negative
+ */
+int print_sign(int n)
+{
+	int i:
+	
+	if (n > 0)
+	{
+		i = 1;
+		_putchar('+');
+	}
+	else if (n < 0)
+	{
+		i = -1;
+		_putchar('-');
+	}
+	else
+	{
+		i = 0;
+		_putchar('0');
+	}
+	return (i);
+}
