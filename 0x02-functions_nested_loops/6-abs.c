8bd026214728012497cd2001216f845bedc1c39a
delexbrainz <alukobamidele@gmail.com>
2022-09-15 13:00:56 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - print absolut value of input
+ * @k: integer to be converted
+ *
+ * Return: the absolute value of function
+ */
+int _abs(int k)
+{
+	if (k < 0)
+		k *= -1;
+	return (k);
+}
