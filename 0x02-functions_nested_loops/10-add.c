797bf1b2a5a66090ad9b7670e5d83d4a5e61bd8a
delexbrainz <alukobamidele@gmail.com>
2022-09-15 23:39:13 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers
+ * @a: first interger input
+ * @b: integer input
+ *
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a +b;
+	return (c);
+}
