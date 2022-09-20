f8260acb7e905db1563de056a3b2271c23b1e6af
delexbrainz <alukobamidele@gmail.com>
2022-09-20 14:54:32 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,11 +7,12 @@
  *
  * Return: destination
  */
+
 char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
 
-	while (*(src * inc) != '\0')
+	while (*(src + inc) != '\0')
 	{
 		*(dest + inc) = *(src + inc);
 		inc++
